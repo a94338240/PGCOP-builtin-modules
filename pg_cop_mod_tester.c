cab869db17f196942964a00bd73be83efcb09906
David <david@pocograph.com>
2013-03-08 17:40:52 +0800

    Rewrite modules.

---
+++ pg_cop_mod_tester.c
@@ -0,0 +1,82 @@
+/*
+    PGCOP - PocoGraph Component Oriented Platform.
+    Copyright (C) 2013  David Wu <david@pocograph.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "pg_cop_modules.h"
+#include "pg_cop_interface.h"
+#include <string.h>
+#include <stdlib.h>
+
+static int tester_init(int argc, char **argv);
+static void *tester_start(pg_cop_module_t *module);
+static char *tester_ping(char *param);
+
+const pg_cop_module_hooks_t pg_cop_module_hooks = {
+  .init = tester_init,
+  .start = tester_start
+};
+
+const pg_cop_module_info_t pg_cop_module_info = {
+  .name = "mod_tester"
+};
+
+static int tester_init(int argc, char **argv) 
+{
+  MOD_DEBUG_INFO("Module init OK");
+  return 0;
+}
+
+static void *tester_start(pg_cop_module_t *module)
+{
+  char *method;
+  char response[128] = {0};
+  char *res;
+  char *param1;
+  const char *methods[] = {"pong",
+                           NULL};
+
+  pg_cop_module_interface_t *intf = 
+    pg_cop_module_interface_announce(module, VSTACK_SCOPE_THREAD);
+
+  for (;;) {
+    MOD_DEBUG_INFO("Wait a ping.");
+    if (pg_cop_module_interface_wait(intf, &method) != 0)
+      return NULL;
+
+    if (strcmp(method, "ping") == 0) {
+      pg_cop_module_interface_pop(intf, VSTACK_TYPE_STRING, &param1);
+      res = tester_ping(param1);
+      pg_cop_module_interface_return(intf, 1, VSTACK_TYPE_STRING, res);
+      MOD_DEBUG_INFO("Give a pong.");
+      free(param1);
+      free(res);
+    } else {
+      MOD_DEBUG_ERROR("No that method named %s.", method);
+      pg_cop_module_interface_return(intf, 0);
+    }
+  
+    free(method);
+  }
+  return NULL;
+}
+
+static char *tester_ping(char *param)
+{
+  char *res = (char *)malloc(128);
+  sprintf(res, "pong %s", param);
+  return res;
+}
